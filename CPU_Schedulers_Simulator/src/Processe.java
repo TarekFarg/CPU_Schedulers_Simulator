b7c3782380c652b8df69ccb71ff4503d6075ab03
TarekFarg <tm747632@gmail.com>
2024-12-08 13:18:17 +0200

    Update Processe Calass

--- CPU_Schedulers_Simulator/src/Processe.java
+++ CPU_Schedulers_Simulator/src/Processe.java
@@ -5,12 +5,63 @@ public class Processe {
     int BurstTime ;
     int PriorityNumber ;
     int WaitingTime ;
-    Processe(String Name , int Color , int ArrivalTime , int BurstTime , int PriorityNumber){
+    int ExecutionOrder;
+    int TurnaroundTime;
+    int QuantumTime ;
+    Processe(String Name , int Color , int ArrivalTime , int BurstTime , int PriorityNumber , int QuantumTime){
         this.Name = Name ;
         this.Color = Color;
         this.ArrivalTime = ArrivalTime ;
         this.BurstTime = BurstTime ;
         this.PriorityNumber = PriorityNumber ;
-        WaitingTime = 0 ;
+        this.QuantumTime = QuantumTime ;
+    }
+    public void setWaitingTime(int t)
+    {
+        WaitingTime = t ;
+    }
+    public void setTurnaroundTime(int t)
+    {
+        TurnaroundTime = t ;
+    }
+    public void setExecutionOrder(int t)
+    {
+        ExecutionOrder = t ;
+    }
+    public int getColor()
+    {
+        return Color;
+    }
+    public String getName()
+    {
+        return Name;
+    }
+    public int getArrivalTime()
+    {
+        return ArrivalTime;
+    }
+    public int getPriorityNumber()
+    {
+        return PriorityNumber ;
+    }
+    public int getBurstTime()
+    {
+        return BurstTime;
+    }
+    public int getWaitingTime()
+    {
+        return WaitingTime;
+    }
+    public int getExecutionOrder()
+    {
+        return ExecutionOrder;
+    }
+    public int getTurnaroundTime()
+    {
+        return TurnaroundTime;
+    }
+    public int getQuantumTime()
+    {
+        return QuantumTime;
     }
 }
