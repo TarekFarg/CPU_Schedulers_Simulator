17560c35977c027be9ffb5dfff03ca3b1c26daf6
TarekFarg <tm747632@gmail.com>
2024-12-08 12:48:48 +0200

    Update Processe Class

--- CPU_Schedulers_Simulator/src/Processe.java
+++ CPU_Schedulers_Simulator/src/Processe.java
@@ -1,3 +1,16 @@
 public class Processe {
-    
+    String Name ;
+    int Color ;
+    int ArrivalTime ;
+    int BurstTime ;
+    int PriorityNumber ;
+    int WaitingTime ;
+    Processe(String Name , int Color , int ArrivalTime , int BurstTime , int PriorityNumber){
+        this.Name = Name ;
+        this.Color = Color;
+        this.ArrivalTime = ArrivalTime ;
+        this.BurstTime = BurstTime ;
+        this.PriorityNumber = PriorityNumber ;
+        WaitingTime = 0 ;
+    }
 }
